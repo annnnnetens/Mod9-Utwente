7c005c7204778d3a31adf69ed7eefa59e3745499
Anete <a.valnere@student.utwente.nl>
2021-10-28 16:25:07 +0200

    changed the emg_reading to sensor, added parameters for filter in state_machine, changed the emg names in state_functions

--- sensor.py
+++ sensor.py
@@ -1,24 +1,55 @@
-from biorobotics import AnalogIn
-import pins
+from biorobotics import AnalogIn, SerialPC
+from pins import Pins
 from BlueSwitch import BlueSwitch
+from biquad_filter import Biquad
 
 
 class SensorState:
-    def __init__(self):
+    def __init__(self, listlowpass, gainlowpass, listbandstop, gainbandstop):
         # TODO: what sensors are available to us?
-        # Not sure if we are reading biceps and triceps. Feel free to change
-        self.EMG_biceps = 0
-        self.EMG_triceps = 0
+        self.emg1_value = 0
+        self.emg2_value = 0
         self.motor_sensor = 0
         self.switch_value = 0
-        # We are using potmeters right?
-        self.potmeter1 = AnalogIn(pins.Pins.POTMETER_1)
-        self.potmeter2 = AnalogIn(pins.Pins.POTMETER_2)
+        
+        # for filtering the EMG
+        self.LP_100_10_1 = Biquad(listlowpass)
+        self.gain_1 = gainlowpass
+        self.LP_100_10_2 = Biquad(listbandstop)
+        self.gain_2 = gainbandstop
+
+        # need this PC to write to uscope
+        self.pc = SerialPC(Pins.SERIAL_PC)
+
+        self.emg1 = AnalogIn(Pins.EMG_1)
+        self.emg2 = AnalogIn(Pins.EMG_2)
+
+        self.potmeter1 = AnalogIn(Pins.POTMETER_1)
+        self.potmeter2 = AnalogIn(Pins.POTMETER_2)
         self.blueswitch = BlueSwitch()
 
-    def update(self):
-        # TODO: read sensor values and update them.
-        # TODO: add encoder to this.
+    def update(self, USE_POTMETERS=True):
         self.switch_value = self.blueswitch.value()
-        self.EMG_biceps = self.potmeter1.read()
-        self.EMG_triceps = self.potmeter2.read()
+            # TODO: read sensor values and update them.
+            # TODO: add encoder to this.
+
+        if USE_POTMETERS:
+            self.emg1_value = self.potmeter1.read()
+            self.emg2_value = self.potmeter2.read()
+        else:
+            # TODO: the values need to be adjusted to be in range of [0, 1] or something
+            # TODO: why is the filter implemented in send_to_pc? where does it come into play
+            self.emg1_value = self.emg1.read()
+            self.emg2_value = self.emg2.read()
+
+
+    def send_to_pc(self):
+        # to use for graphing in uscope, maybe needs a ticker?
+        # doubles over the emg.read() with update() but since this is only for debugging, it should be okay
+        
+        for i, emg in enumerate(self.emgs):
+            self.pc.set(i, emg.read())
+            
+        
+        self.pc.set(2, self.gain_1*self.LP_100_10_1.filter(emg.read()))
+        self.pc.send()
