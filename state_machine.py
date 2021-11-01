d447d666a2e148e1af493c55cb670552f45f6df1
amber <a.l.plakke@student.utwente.nl>
2021-11-01 12:59:40 +0100

    change filter coeficients

--- state_machine.py
+++ state_machine.py
@@ -7,14 +7,14 @@ from state_functions import StateFunctions
 
 class StateMachine:
     def __init__(self, ticker_freq):
-        # 10Hz cut off Butterworth low pass - for EMG
-        listlowpass = [-1.705552145544083852968242354108951985836,   0.743655195048865791385139800695469602942, 1.0, 2.0, 1.0]
+        # 6Hz cut off, sample frequency 1000 Hz, order 2 Butterworth low pass  - for EMG
+        listlowpass = [-1.9467,   0.9481, 0.000346, 0.0006921, 0.000346]
         gainlowpass = 0.009525762376195455113925270040908799274
         # 48-52 band stop Butterworth - for EMG
         listbandstop = [-0.960616192564186621716260106040863320231,  0.919547137907040124105151335243135690689, 1.0, -1.000877940003687793790732030174694955349, 1.0]
         gainbandstop = 0.959773568953520062052575667621567845345
 
-        self.USE_POTMETERS = True
+        self.USE_POTMETERS = False
 
         self.robot_state = State()
         self.sensor_state = SensorState()
